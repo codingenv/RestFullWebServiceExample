7975dfcd51289d98726576122b6660455736f928
Prakash <apps.prak@gmail.com>
2023-05-07 02:28:50 +0530

    Configured H2 console successfully. https://www.youtube.com/watch?v=PSrHcCwvfVQ

--- rest/src/main/java/com/basic/rest/project/rest/RestApplication.java
+++ rest/src/main/java/com/basic/rest/project/rest/RestApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @ComponentScan
 @Slf4j
-@EntityScan("com.basic.rest.project.rest")
+//@EntityScan("com.basic.rest.project.rest")
 
 
 
@@ -19,7 +20,9 @@ public class RestApplication {
 
 	public static void main(String[] args) {
 		log.info("========Starting Rest Application=========");
-		SpringApplication.run(RestApplication.class, args);
+		ConfigurableApplicationContext context =  SpringApplication.run(RestApplication.class, args);
+		Object dataSource = context.getBean("dataSource");
+		System.out.println(dataSource);
 		log.info("========Stopping Rest Application=========");
 	}
 
